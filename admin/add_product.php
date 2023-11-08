eac9b481cbfa69cf04c2d13501132daaadb9d6a5
PatricColaco <135795966+PatricColaco@users.noreply.github.com>
2023-11-08 22:39:51 +0530

    s

--- admin/add_product.php
+++ admin/add_product.php
@@ -1,11 +1,11 @@
 <?php
 include('../includes/connect.php');
 if (isset($_POST['add_product'])) {
-    $shoe_name = $_POST['p_name'];
-    $shoe_desc = $_POST['p_desc'];
-    $shoe_supplier = $_POST['s_id'];
-    $shoe_brand = $_POST['b_id'];
-    $shoe_price = $_POST['p_price'];
+  $shoe_name = $_POST['mname'];
+  $shoe_desc = $_POST['mdesc'];
+  $shoe_seller = $_POST['sid'];
+  $shoe_brand = $_POST['bid'];
+  $shoe_price = $_POST['mprice'];
 
     $shoe_pic = $_FILES['mimage']['name'];
     $temp_shoe_pic = $_FILES['mimage']['tmp_name'];
@@ -15,7 +15,7 @@ if (isset($_POST['add_product'])) {
         exit();
     }
     else{
-        move_uploaded_file($temp_shoe_pic,"../assets/shoe_Images/$shoe_pic");
+        move_uploaded_file($temp_shoe_pic,"../assets/Shoes/$shoe_pic");
         
         $insert_query = "insert into `product` (p_name,p_price,p_desc,s_id,p_pic,b_id) values ('$shoe_name','$shoe_price','$shoe_desc',$shoe_supplier,'$shoe_pic',$shoe_brand)";
         $result_query = mysqli_query($conn,$insert_query);
@@ -36,9 +36,7 @@ if (isset($_POST['add_product'])) {
         <form
           action=""
           method="post"
-          class="add-product-form"
-          style="color: rgb(81, 81, 81)"
-        >
+          enctype="multipart/form-data">
           <h3>add a new product</h3>
           <input
             type="text"
@@ -47,28 +45,49 @@ if (isset($_POST['add_product'])) {
             class="box"
             required
           />
-          <input
-            type="number"
-            name="p_price"
-            min="0"
-            placeholder="enter the product price"
-            class="box"
-            required
-          />
-          <input
-            type="text"
-            name="p_desc"
-            placeholder="enter the product desc"
-            class="box"
-            required
-          />
-          <input
-            type="file"
-            name="p_image"
-            accept="image/png, image/jpg, image/jpeg"
-            class="box"
-            required
-          />
+          <div>
+        <label for="sname" class="form-label">Enter Supplier</label>
+        <select class="form-select mb-3" id="sname" aria-label="Default select example" name="sid">
+            <option selected></option>
+            <?php
+            include('../includes/connect.php');
+            $select_query = "select * from `seller`;";
+            $result_query = mysqli_query($conn, $select_query);
+            while ($row = mysqli_fetch_assoc($result_query)) {
+                $supplier_id = $row['s_id'];
+                $supplier_name = $row['s_name'];
+                echo "<option value='$supplier_id'>$supplier_name</option>";
+            }
+            ?>
+        </select>
+    </div>
+          <label for="bname" class="form-label">Enter Brand</label>
+        <select class="form-select mb-3" id="bname" aria-label="Default select example" name="bid">
+            <option selected></option>
+            <?php
+            include('../includes/connect.php');
+            $select_query = "select * from `brand`;";
+            $result_query = mysqli_query($conn, $select_query);
+            while ($row = mysqli_fetch_assoc($result_query)) {
+                $brand_id = $row['b_id'];
+                $brand_name = $row['b_name'];
+                echo "<option value='$brand_id'>$brand_name</option>";
+            }
+            ?>
+        </select>
+    </div>
+    <label for="floatingInputGroup2" class="form-label">Mobile Price</label>
+    <div class="input-group mb-3">
+        <span class="input-group-text">&#8377;</span>
+        <div class="form-floating">
+            <input type="text" class="form-control" id="floatingInputGroup2" placeholder="Price" name="mprice">
+            <label for="floatingInputGroup2">Price</label>
+        </div>
+    </div>
+    <div class="mb-3">
+        <label for="formFile" class="form-label">Mobile Image</label>
+        <input class="form-control" type="file" id="formFile" name="mimage">
+    </div>
           <input
             type="submit"
             value="add the product"
--- admin/index.php
+++ admin/index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+  <!--  
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -11,7 +12,44 @@
     <link rel="stylesheet" href="style.css" />
   </head>
   <body>
+--><head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"
+        integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../style.css">
+    <style>
+    /* footer {
+        position: absolute;
+        bottom: 0;
+        right: 0;
+        left: 0;
+    } */
+    </style>
+</head>
 
+<body class="bg-dark-subtle">
+    <div class="container-fluid p-0">
+        <nav class="navbar navbar-expand-lg navbar-light bg-dark ">
+            <div class="container-fluid">
+                <img src="#" alt="Logo" class="logo">
+                <nav class="navbar navbar-expand-lg">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a href="" class="nav-link">Welcome Guest</a>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </nav>
+
+        <div class="container-fluid bg-dark d-flex justify-content-center ">
+            <h2 class="text-light">Manage Shoes</h2>
+        </div>
     <?php 
     include("header.php");
     ?>
@@ -59,8 +97,41 @@
             value="add the product"
             name="add_product"
             class="btn"
-          />
-        </form>  -->
+          />  -->
+          <div class="row pb-2 bg-dark-subtle ">
+            <div class="col-md-12 bg-dark-subtle d-flex flex-column align-items-center gap-4 pt-4">
+                <a href="">
+                    <img src="../assets/images/samsung_galaxy_ultra.jpg" alt="Admin Image" width="250px" height="250px">
+                </a>
+                <h5 class="text-center">KICK_GAME</h5>
+                <div class="button text-center d-flex gap-2">
+                    <button><a href="./index.php?add_product" class="nav-link text-light bg-dark p-2">Add Shoe</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">View Shoes</a></button>
+                    <button><a href="./index.php?add_supplier" class="nav-link text-light bg-dark p-2">Add Supplier</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">View Suppliers</a></button>
+                    <button><a href="./index.php?add_brand" class="nav-link text-light bg-dark p-2">Add Brand</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">View Brands</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">All Orders</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">All Payments</a></button>
+                    <button><a href="" class="nav-link text-light bg-dark p-2">List Users</a></button>
+                    <button class="bg-danger"><a href="" class="nav-link text-light bg-danger p-2">Logout</a></button>
+                </div>
+            </div>
+        </div>
+        </form>  
+        <?php
+                if(isset($_GET['add_product'])){
+                  include('add_product.php');
+                }
+                if(isset($_GET['add_supplier'])){
+                    include('add_supplier.php');
+                }
+                if(isset($_GET['add_brand'])){
+                    include('add_brand.php');
+                }
+                
+        ?>
+        
       </section>
     </div>
     <script src="script.js"></script>
