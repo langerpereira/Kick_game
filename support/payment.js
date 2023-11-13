b3246c8080245fe38b3c255e4b9c7600ea0cc1c7
Langer Pereira <langerpereira12@gmail.com>
2023-11-13 23:26:18 +0530

    support page

--- support/payment.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const parallaxContainer = document.querySelector('.parallax-container');
-
-    window.addEventListener('scroll', function () {
-        const scrolled = window.scrollY;
-        parallaxContainer.style.transform = `translateY(${scrolled * 0.5}px)`;
-    });
-});
